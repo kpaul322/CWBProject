1623d55af6eea0f60f5ee5018db9bb5b4e662732
kpaul322 <101074942+kpaul322@users.noreply.github.com>
2022-03-29 22:14:51 -0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,45 @@
+$errors = '';
+$myemail = 'kg32284@outlook.com';
+if(empty($_POST['fname'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['lname']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+
+$name = $_POST['fname']; 
+$email_address = $_POST['email']; 
+$message = $_POST['lname']; 
+
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
+if( empty($errors))
+
+{
+
+$to = $myemail;
+
+$email_subject = "Contact form submission: $name";
+
+$email_body = "You have received a new message. ".
+
+" Here are the details:\n Name: $name \n ".
+
+"Email: $email_address\n Message \n $message";
+
+$headers = "From: $myemail\n";
+
+$headers .= "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+
+//redirect to the 'thank you' page
+
+header('Location: contact.html');
+
+}
